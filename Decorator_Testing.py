395a0a7fd38e4d4eb75f2bd12754b147c1302363
Neelam Singhal <64087828+Neelam-Singhal@users.noreply.github.com>
2022-08-03 11:29:15 +0530

    update: Decorator function

--- Decorator_Testing.py
+++ Decorator_Testing.py
@@ -3,15 +3,26 @@
 # then returns another function
 # All of this is done without altering the source code of the original function that you passed in
 
+# Why to use Decorator -> Add functionaly to existing function without changing the existing code
 def decorator_function(original_function): # Passing in a function as an argument
     def wrapper_function():
+        print("This function is executed before {} function".format(original_function.__name__))
         return original_function()  # Executing the original function. May add new functionality to it
     return wrapper_function  # Retuning a new function
 
 
+
 def display():
-    print( "Display function")
+    print( "Run Display function")
 
 decorator_variable = decorator_function(display)
-
 decorator_variable()    
+
+# @decorator_function means --> display = decorator_function(display)
+# Essentially, the output of display() has been updated without actually changing the code for display() function and instead using a decorator.
+
+@decorator_function
+def display():
+    print( "Run Display function")
+
+display()
